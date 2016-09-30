10b0a327587202fd1ad0db024ca02f46ac722356
Sven van der Zee <svenzee@hotmail.nl>
2016-09-30 10:16:52 +0200

    Add files via upload
    CRUD operations and JSON data

---
+++ crud/connect.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: svenvdz
+ * Date: 16-9-2016
+ * Time: 10:10
+ */
+class connect
+{
+
+    private $conn;
+
+    function __construct()
+    {
+
+    }
+
+    function connect(){
+        $this->conn = new mysqli("","","","");
+        if(mysqli_connect_errno()){
+            echo "something went wrong, please try again";
+        }
+        return $this->conn;
+    }
+
+}
